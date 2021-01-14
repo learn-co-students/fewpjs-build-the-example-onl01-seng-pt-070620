f2c85450492282a8af5820fc0ebdf00e4fc0e467
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-14 15:37:40 -0800

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+let errorModel = document.getElementById("modal")
+errorModel.classList.add("hidden")
 
 
 //------------------------------------------------------------------------------
