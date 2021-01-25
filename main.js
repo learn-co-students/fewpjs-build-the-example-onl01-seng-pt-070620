a81923720a943d0929ab2e30eb4dd730a73211ef
kam3r0nz <kam.hungerford@gmail.com>
2021-01-24 18:05:50 -0600

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,33 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+window.addEventListener('DOMContentLoaded', () => {
+  let errorModule = document.getElementById('modal')
+  errorModule.className = "hidden"
+});
 
-
-
+let heartIcon = document.getElementsByClassName('like-glyph')[0]
+heartIcon.addEventListener('click', function() {
+  mimicServerCall()
+  .then(function() {
+    if (heartIcon.innerTEXT == EMPTY_HEART) {
+      heartIcon.innerTEXT == FULL_HEART
+      heartIcon.className = "activated-heart"
+    } else {
+        heartIcon.innerText = EMPTY_HEART
+        heartIcon.classList.remove("activated-heart")
+      }
+    })
+    .catch((error) => {
+      const modal = document.getElementById("modal")
+      const message = document.querySelector("p#modal-message")
+      message.innerText = error
+      modal.classList.remove("hidden")
+      window.setTimeout(() => {
+        modal.className = "hidden"
+      }, 5000)
+    })
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
