b01f55f69538307716e3ff1281bc7844afa920f1
kinnari <kinnari1022@yahoo.com>
2021-01-28 12:57:50 -0500

    Done.

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+  document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.querySelector('#modal')
+    const hearts = document.getElementsByClassName("like-glyph")
+    likePost(hearts);
+  })
+    const likePost = (hearts) => {
+    for (const heart of hearts){
+      heart.addEventListener("click", (e) => {
+        mimicServerCall()
+        .then(() => {
+          if (heart.innerHTML == EMPTY_HEART){
+            heart.innerHTML = FULL_HEART
+            heart.className = "activated-heart"
+          } else {
+            heart.innerHTML = EMPTY_HEART
+            heart.className = "like-glyph"
+          }
+        
+        })
+        .catch(error =>{
+          modal.hidden = false
+          const modalMessage = document.querySelector("#modal-message")
+          modalMessage.innerText = error
+          setTimeout(() =>{
+            modal.hidden = true
+          }, 5000)
+        })
+      })
+    }
+  }
+ 
 // Your JavaScript code goes here!
 
 
@@ -13,6 +43,7 @@ const FULL_HEART = '♥'
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
+    
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
